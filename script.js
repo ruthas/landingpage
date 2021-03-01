e13afd623b0af221a119bfbf30fcb2cf8249bde5
ruthas <62007410+ruthas@users.noreply.github.com>
2021-03-01 14:04:26 +0200

    moved js to script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // const imgSlider = (anything) => {
 //     document.querySelector('.starbucks').src = anything
 // }
+
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything
+}
+
+function changeCircle(colour) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = colour;
+}
